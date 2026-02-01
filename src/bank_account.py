8760c66bbd642f81abe9740a4ddd5f4328a17eeb
ramossinflores <mlaura.ragra@gmail.com>
2026-02-01 21:09:34 +0100

    refactor(bank_account):añade  prevención de sobregiros y raise para en cantidades inválidas

--- src/bank_account.py
+++ src/bank_account.py
@@ -7,16 +7,28 @@ class BankAccount:
     
     # método depósito 
     def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
+        if amount <= 0:
+            raise ValueError('El depósito debe ser mayor y distinto de 0')
+        self.balance += amount
         return self.balance
     
     # método retiro
     def withdraw(self, amount):
-        if amount > 0: 
-            self.balance -= amount
+        if amount <= 0:
+            raise ValueError('El retiro debe ser mayor y distinto de 0')
+        if amount > self.balance:
+            raise ValueError('El saldo en su cuenta no es suficiente para realizar este retiro')
+        self.balance -= amount
         return self.balance
     
     # getter balance
     def get_balance(self): 
         return self.balance
+    
+    # método transferencia
+    def transfer(self, amount):
+        if amount < self.balance: 
+            raise ValueError(f'No es posible transferir {amount}. Saldo disponible: {self.balance}.')
+        if amount <=0:
+            raise ValueError('La cantidad debe ser mayor y distinta de cero')
+        self.balance -= amount
