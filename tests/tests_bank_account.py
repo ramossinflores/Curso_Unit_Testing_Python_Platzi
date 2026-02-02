0bafc3bba3365d7524d99372882240bf1822c805
ramossinflores <mlaura.ragra@gmail.com>
2026-02-02 10:16:46 +0100

    test(bank-account): añadir pruebas para validaciones y excepciones

--- tests/tests_bank_account.py
+++ tests/tests_bank_account.py
@@ -10,14 +10,39 @@ class BankAccountTests(unittest.TestCase):
         new_balance = self.account.deposit(500)
         # assert new_balance == 1500
         self.assertEqual(new_balance,1500)
+
+    def test_invalid_amount_deposit(self):
+        with self.assertRaises(ValueError):
+            self.account.deposit(0)
+
     def test_withdraw(self):
         new_balance = self.account.withdraw(200)
         #assert new_balance == 800
         self.assertEqual(new_balance,800)
+    
+    def test_insufficient_funds_withdraw(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw(2000)
+    
+    def test_invalid_amount_withdraw(self):
+        with self.assertRaises(ValueError):
+            self.account.withdraw(0)
 
     def test_get_balance(self):
         # assert self.account.get_balance() == 1000
         self.assertEqual(self.account.get_balance(), 1000)
+
+    def test_transfer(self):
+        new_balance = self.account.transfer(300)
+        self.assertEqual(new_balance, 700)
+    
+    def test_insufficient_funds_transfer(self):
+        with self.assertRaises(ValueError):
+            self.account.transfer(1200)
+
+    def test_invalid_amount_transfer(self):
+        with self.assertRaises(ValueError):
+            self.account.transfer(0)
 
 if __name__== '__main__':
     unittest.main()
