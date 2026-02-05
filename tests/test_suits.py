8da967a2ce2336786d11feb94a3b4fed9e8b3363
ramossinflores <mlaura.ragra@gmail.com>
2026-02-05 15:37:48 +0100

    test: normalizar nombres a patrón test_*.py

---
+++ tests/test_suits.py
@@ -0,0 +1,17 @@
+import unittest
+from tests.test_bank_account import BankAccountTests
+
+def bank_account_suite():
+    suite = unittest.TestSuite() # Crea una suit 
+    suite.addTest(BankAccountTests("test_deposit")) # y además agrega estas pruebas
+    suite.addTest(BankAccountTests("test_withdraw"))
+    return suite 
+
+if __name__ == "__main__": 
+    runner = unittest.TextTestRunner() # Se crea un runner para correr las pruebas
+    runner.run(bank_account_suite())
+
+# PYTHONPATH=. python .\tests\tests_suits.py ----> en Linux
+# $env:PYTHONPATH="." ----> En PowerShell
+# python .\tests\tests_suits.py
+
