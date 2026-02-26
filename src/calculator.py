4c93b84d9fa1f57da3a7771c679926373685277b
ramossinflores <mlaura.ragra@gmail.com>
2026-02-26 11:05:16 +0100

    feat(calculator): Añadir pruebas de código con doctests

--- src/calculator.py
+++ src/calculator.py
@@ -7,6 +7,10 @@
 #====================================================================
 
 def add(a,b):
+    """
+    >>> add(5,7)
+    12
+    """
     return a + b
 
 def subtract(a,b): 
@@ -16,9 +20,13 @@ def multiply(a,b):
     return a * b
 
 def divide(a,b):
+    """
+    >>> divide(10,0)
+    Traceback (most recent call last):
+    ...
+    ZeroDivisionError: La división entre cero no es posible
+    """
     if b == 0: 
        raise ZeroDivisionError('La división entre cero no es posible') 
     return a / b
 
-valor = divide(8,0)
-print(valor)
