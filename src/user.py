ad8a4baaa9eee6ab99ab1d37328aec1c0545c3dd
ramossinflores <mlaura.ragra@gmail.com>
2026-02-26 13:03:54 +0100

    fix: Corregir typo

---
+++ src/user.py
@@ -0,0 +1,16 @@
+import src.bank_account
+from src.calculator import add
+
+class User: 
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email 
+        self.accounts = []
+
+    def add_account(self, account):
+        self.accounts.append(account)
+
+    def get_total_balance(self,accounts):
+        return add(self.get_balance() for account in self.accounts)
+
+        
